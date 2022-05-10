790f19cbe7946b36a2d8bcb0ddf5cd4e4e23c51a
Lood <loodvn@gmail.com>
2022-05-10 15:01:56 -0400

    also kicking off small batch MSA 7

--- scripts/2022_04_26_deepseq_newmsa.sh
+++ scripts/2022_04_26_deepseq_newmsa.sh
@@ -12,7 +12,7 @@
 #SBATCH --mail-type=TIME_LIMIT_80,TIME_LIMIT,FAIL,ARRAY_TASKS
 #SBATCH --mail-user="lodevicus_vanniekerk@hms.harvard.edu"
 
-#SBATCH --job-name="eve_deepseq_v6_bigbatch"
+#SBATCH --job-name="eve_deepseq_v6"
 
 # Job array-specific
 #SBATCH --output=logs/slurm_files/slurm-lvn-%A_%3a-%x.out   # Nice tip: using %3a to pad to 3 characters (23 -> 023)
@@ -57,7 +57,7 @@ export MSA_list='/n/groups/marks/users/lood/EVE/data/mappings/eve_msa_mapping_20
 export MSA_weights_location='/n/groups/marks/users/lood/EVE/data/weights_msa_tkmer_20220227/'
 export VAE_checkpoint_location='/n/groups/marks/users/lood/EVE/results/VAE_parameters_v5_20220227/'
 export model_name_suffix='2022_04_26_DeepSeq_reproduce'  # Essential for skip_existing to work # Copied from O2  # TODO Should make '2022_04_26_DeepSeq_msa_v6'
-export model_parameters_location='./EVE/deepseq_model_params_bigbatch.json'
+export model_parameters_location='./EVE/deepseq_model_params.json'
 export training_logs_location='./logs/'
 export protein_index=${SLURM_ARRAY_TASK_ID}
 
