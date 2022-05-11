22ff088360c7e3da279bdfd9db0e17699ddea780
Lood <loodvn@gmail.com>
2022-05-11 13:56:34 -0400

    trying batch aggregation

--- scripts/2022_04_26_deepseq_newmsa_Step2_compute_evol_indices_DMS_ARC.sh
+++ scripts/2022_04_26_deepseq_newmsa_Step2_compute_evol_indices_DMS_ARC.sh
@@ -68,7 +68,7 @@ export protein_index=${SLURM_ARRAY_TASK_ID}
 export computation_mode='DMS'
 #export all_singles_mutations_folder='./data/mutations'
 export mutations_location='/data/coml-ecr/grte2996/EVE/DMS/DMS_Benchmarking_Dataset_v5_20220227_old'
-export output_evol_indices_location='./results/evol_indices_20220501_v5_memory'  # Experimental output location
+export output_evol_indices_location='./results/evol_indices_20220501_v5_memory/batch'  # Experimental output location
 export output_evol_indices_filename_suffix='_2022_04_26_DeepSeq_reproduce_v6'
 export num_samples_compute_evol_indices=20000
 export batch_size=1024  # Pushing batch size to limit of GPU memory
@@ -85,5 +85,6 @@ python compute_evol_indices_DMS.py \
     --mutations_location ${mutations_location} \
     --output_evol_indices_location ${output_evol_indices_location} \
     --num_samples_compute_evol_indices ${num_samples_compute_evol_indices} \
-    --batch_size ${batch_size}
+    --batch_size ${batch_size} \
+    --aggregation_method "batch"
 #    --skip_existing  # Don't skip for experiments measuring runtime
