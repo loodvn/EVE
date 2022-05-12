2a36a670a8cb79b8a99ae353f7e8a708a5f0426e
Lood <loodvn@gmail.com>
2022-05-11 21:23:43 -0400

    relaunch the big batch jobs, they're stalling

--- scripts/2022_04_26_deepseq_newmsa_Step2_compute_evol_indices_DMS_ARC.sh
+++ scripts/2022_04_26_deepseq_newmsa_Step2_compute_evol_indices_DMS_ARC.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 ##SBATCH --constraint=gpu_sku:A100  #|gpu_sku:RTX-A6000
 ##SBATCH --constraint='gpu_mem:40GB|gpu_mem:48GB'
-#SBATCH --mem=160G                          # Memory total in MB (for all cores)
+#SBATCH --mem=100G                          # Memory total in MB (for all cores)
 
 # ARC
 #SBATCH --partition=short
