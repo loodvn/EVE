dc931ef5416119785bb79cdaa0d8926afcad6335
Lood <loodvn@gmail.com>
2022-05-11 23:04:37 -0400

    still using job arrays

--- scripts/2022_05_11_evol_indices_custom_SPG1_DMS_O2.sh
+++ scripts/2022_05_11_evol_indices_custom_SPG1_DMS_O2.sh
@@ -17,7 +17,7 @@
 # Job array-specific
 #SBATCH --output=./logs/slurm_files/slurm-lvn-%A_%3a-%x.out   # Nice tip: using %3a to pad to 3 characters (23 -> 023)
 ##SBATCH --error=./logs/slurm_files/slurm-lvn-%A_%3a-%x.err   # Optional: Redirect STDERR to its own file
-##SBATCH --array=0-9  # Custom: Only SPG1 mapping
+#SBATCH --array=0-9  # Custom: Only SPG1 mapping
 #SBATCH --hold  # Holds job so that we can first manually check a few
 
 # Quite neat workflow:
