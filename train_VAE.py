c8249f0b74a9e08bf63b8dd4310999d490ab9138
Lood van Niekerk (HMS O2) <lood_vanniekerk@hms.harvard.edu>
2023-09-13 05:29:21 -0400

    Training: Added some checks to input/output files

--- train_VAE.py
+++ train_VAE.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
         help="Force loading of weights from MSA_weights_location (useful if you want to make sure you're using precalculated weights). Will fail if weight file doesn't exist.",
         default=False)
     parser.add_argument("--overwrite_weights", help="Will overwrite weights file if it already exists", action="store_true", default=False)
+    parser.add_argument("--skip_existing", help="Will quit gracefully if model checkpoint file already exists", action="store_true", default=False)
 
     args = parser.parse_args()
 
@@ -33,6 +34,8 @@ if __name__ == '__main__':
 
     assert os.path.isfile(args.MSA_list), f"MSA file list {args.MSA_list} doesn't seem to exist"
     mapping_file = pd.read_csv(args.MSA_list)
+    if mapping_file["protein_name"].duplicated().any():
+        print(f"Note: Duplicate protein_name detected in the mapping file. Files may be overwritten - please make sure to use unique values for protein_name.")
     protein_name = mapping_file['protein_name'][args.protein_index]
     msa_location = args.MSA_data_folder + os.sep + mapping_file['msa_location'][args.protein_index]
     print("Protein name: " + str(protein_name))
@@ -47,6 +50,17 @@ if __name__ == '__main__':
             print("Couldn't load theta from mapping file. Using default value of 0.2")
             theta = 0.2
 
+    model_name = protein_name + "_" + args.model_name_suffix
+    print("Model name: " + str(model_name))
+    model_checkpoint_final_path = args.VAE_checkpoint_location + os.sep + model_name + "_final"
+    if os.path.isfile(model_checkpoint_final_path):
+        if args.skip_existing:
+            print("Weights file already exists, skipping, since --skip_existing was specified")
+            exit(0)
+        else:
+            raise FileExistsError(f"Model checkpoint {model_checkpoint_final_path} already exists. \
+                                  Use --skip_existing to skip without raising an error, or delete the destination file if you want to rerun.")
+
     # Using data_kwargs so that if options aren't set, they'll be set to default values
     data_kwargs = {}
     if args.threshold_focus_cols_frac_gaps is not None:
@@ -59,11 +73,19 @@ if __name__ == '__main__':
 
     print("Theta MSA re-weighting: " + str(theta))
 
-    weights_file = args.MSA_weights_location + os.sep + protein_name + '_theta_' + str(theta) + '.npy'
+    # Load weights file if it's in the mapping file
+    if "weight_file_name" in mapping_file.columns:
+            weights_file = args.MSA_weights_location + os.sep + mapping_file["weight_file_name"][args.protein_index]
+            print("Using weights filename from mapping file")
+    else:
+        weights_file = args.MSA_weights_location + os.sep + protein_name + '_theta_' + str(theta) + '.npy'
+    
+    print(f"Weights location: {weights_file}")
+
     if args.force_load_weights:
         print("Flag force_load_weights enabled - Forcing that we use weights from file:", weights_file)
         if not os.path.isfile(weights_file):
-            raise FileNotFoundError(f"Weights file {weights_file} doesn't exist. "
+            raise FileNotFoundError(f"Weights file {weights_file} doesn't exist."
                                     f"To recompute weights, remove the flag --force_load_weights.")
 
     data = data_utils.MSA_processing(
@@ -73,9 +95,7 @@ if __name__ == '__main__':
         weights_location=weights_file,
         **data_kwargs,
     )
-
-    model_name = protein_name + "_" + args.model_name_suffix
-    print("Model name: " + str(model_name))
+    # TODO Note that protein_name isn't unique, so we'll have to use DMS id in some cases. Ideally we should have an MSA_id or something
 
     assert os.path.isfile(args.model_parameters_location), args.model_parameters_location
     model_params = json.load(open(args.model_parameters_location))
@@ -96,12 +116,13 @@ if __name__ == '__main__':
 
     model_params["training_parameters"]['training_logs_location'] = args.training_logs_location
     model_params["training_parameters"]['model_checkpoint_location'] = args.VAE_checkpoint_location
+    
 
     print("Starting to train model: " + model_name)
     model.train_model(data=data, training_parameters=model_params["training_parameters"])
 
     print("Saving model: " + model_name)
-    model.save(model_checkpoint=model_params["training_parameters"]['model_checkpoint_location'] + os.sep + model_name + "_final",
+    model.save(model_checkpoint=model_checkpoint_final_path,
                encoder_parameters=model_params["encoder_parameters"],
                decoder_parameters=model_params["decoder_parameters"],
                training_parameters=model_params["training_parameters"]
