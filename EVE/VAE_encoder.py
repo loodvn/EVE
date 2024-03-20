16b8e9398370f2df794b0ed13f636c3526e3dca3
Lood van Niekerk (HMS O2) <lood_vanniekerk@hms.harvard.edu>
2024-03-20 14:54:56 -0400

    Black linting

--- EVE/VAE_encoder.py
+++ EVE/VAE_encoder.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn as nn
 
+
 class VAE_MLP_encoder(nn.Module):
     """
     MLP encoder class for the VAE model.
     """
-    def __init__(self,params):
+
+    def __init__(self, params):
         """
         Required input parameters:
         - seq_len: (Int) Sequence length of sequence alignment
@@ -19,50 +21,50 @@ class VAE_MLP_encoder(nn.Module):
         """
         super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.seq_len = params['seq_len']
-        self.alphabet_size = params['alphabet_size']
-        self.hidden_layers_sizes = params['hidden_layers_sizes']
-        self.z_dim = params['z_dim']
-        self.convolve_input = params['convolve_input']
-        self.convolution_depth = params['convolution_input_depth']
-        self.dropout_proba = params['dropout_proba']
+        self.seq_len = params["seq_len"]
+        self.alphabet_size = params["alphabet_size"]
+        self.hidden_layers_sizes = params["hidden_layers_sizes"]
+        self.z_dim = params["z_dim"]
+        self.convolve_input = params["convolve_input"]
+        self.convolution_depth = params["convolution_input_depth"]
+        self.dropout_proba = params["dropout_proba"]
 
         self.mu_bias_init = 0.1
         self.log_var_bias_init = -10.0
 
-        #Convolving input with kernels of size 1 to capture potential similarities across amino acids when encoding sequences
+        # Convolving input with kernels of size 1 to capture potential similarities across amino acids when encoding sequences
         if self.convolve_input:
-            self.input_convolution = nn.Conv1d(in_channels=self.alphabet_size,out_channels=self.convolution_depth,kernel_size=1,stride=1,bias=False)
+            self.input_convolution = nn.Conv1d(in_channels=self.alphabet_size, out_channels=self.convolution_depth, kernel_size=1, stride=1, bias=False)
             self.channel_size = self.convolution_depth
         else:
             self.channel_size = self.alphabet_size
 
-        self.hidden_layers=torch.nn.ModuleDict()
+        self.hidden_layers = torch.nn.ModuleDict()
         for layer_index in range(len(self.hidden_layers_sizes)):
-            if layer_index==0:
-                self.hidden_layers[str(layer_index)] = nn.Linear((self.channel_size*self.seq_len),self.hidden_layers_sizes[layer_index])
+            if layer_index == 0:
+                self.hidden_layers[str(layer_index)] = nn.Linear((self.channel_size * self.seq_len), self.hidden_layers_sizes[layer_index])
                 nn.init.constant_(self.hidden_layers[str(layer_index)].bias, self.mu_bias_init)
             else:
-                self.hidden_layers[str(layer_index)] = nn.Linear(self.hidden_layers_sizes[layer_index-1],self.hidden_layers_sizes[layer_index])
+                self.hidden_layers[str(layer_index)] = nn.Linear(self.hidden_layers_sizes[layer_index - 1], self.hidden_layers_sizes[layer_index])
                 nn.init.constant_(self.hidden_layers[str(layer_index)].bias, self.mu_bias_init)
-        
-        self.fc_mean = nn.Linear(self.hidden_layers_sizes[-1],self.z_dim)
+
+        self.fc_mean = nn.Linear(self.hidden_layers_sizes[-1], self.z_dim)
         nn.init.constant_(self.fc_mean.bias, self.mu_bias_init)
-        self.fc_log_var = nn.Linear(self.hidden_layers_sizes[-1],self.z_dim)
+        self.fc_log_var = nn.Linear(self.hidden_layers_sizes[-1], self.z_dim)
         nn.init.constant_(self.fc_log_var.bias, self.log_var_bias_init)
 
         # set up non-linearity
-        if params['nonlinear_activation'] == 'relu':
+        if params["nonlinear_activation"] == "relu":
             self.nonlinear_activation = nn.ReLU()
-        elif params['nonlinear_activation'] == 'tanh':
+        elif params["nonlinear_activation"] == "tanh":
             self.nonlinear_activation = nn.Tanh()
-        elif params['nonlinear_activation'] == 'sigmoid':
+        elif params["nonlinear_activation"] == "sigmoid":
             self.nonlinear_activation = nn.Sigmoid()
-        elif params['nonlinear_activation'] == 'elu':
+        elif params["nonlinear_activation"] == "elu":
             self.nonlinear_activation = nn.ELU()
-        elif params['nonlinear_activation'] == 'linear':
+        elif params["nonlinear_activation"] == "linear":
             self.nonlinear_activation = nn.Identity()
-        
+
         if self.dropout_proba > 0.0:
             self.dropout_layer = nn.Dropout(p=self.dropout_proba)
 
@@ -71,12 +73,12 @@ class VAE_MLP_encoder(nn.Module):
             x = self.dropout_layer(x)
 
         if self.convolve_input:
-            x = x.permute(0,2,1) 
+            x = x.permute(0, 2, 1)
             x = self.input_convolution(x)
-            x = x.view(-1,self.seq_len*self.channel_size)
+            x = x.view(-1, self.seq_len * self.channel_size)
         else:
-            x = x.view(-1,self.seq_len*self.channel_size) 
-        
+            x = x.view(-1, self.seq_len * self.channel_size)
+
         for layer_index in range(len(self.hidden_layers_sizes)):
             x = self.nonlinear_activation(self.hidden_layers[str(layer_index)](x))
             if self.dropout_proba > 0.0:
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -5,8 +5,10 @@ import os
 import torch
 import tqdm
 
+
 class MSA_processing:
-    def __init__(self,
+    def __init__(
+        self,
         MSA_location="",
         theta=0.2,
         use_weights=True,
@@ -14,18 +16,18 @@ class MSA_processing:
         preprocess_MSA=True,
         threshold_sequence_frac_gaps=0.5,
         threshold_focus_cols_frac_gaps=0.3,
-        remove_sequences_with_indeterminate_AA_in_focus_cols=True
-        ):
-        
+        remove_sequences_with_indeterminate_AA_in_focus_cols=True,
+    ):
+
         """
         Parameters:
-        - msa_location: (path) Location of the MSA data. Constraints on input MSA format: 
+        - msa_location: (path) Location of the MSA data. Constraints on input MSA format:
             - focus_sequence is the first one in the MSA data
             - first line is structured as follows: ">focus_seq_name/start_pos-end_pos" (e.g., >SPIKE_SARS2/310-550)
             - corespondding sequence data located on following line(s)
             - then all other sequences follow with ">name" on first line, corresponding data on subsequent lines
         - theta: (float) Sequence weighting hyperparameter. Generally: Prokaryotic and eukaryotic families =  0.2; Viruses = 0.01
-        - use_weights: (bool) If False, sets all sequence weights to 1. If True, checks weights_location -- if non empty uses that; 
+        - use_weights: (bool) If False, sets all sequence weights to 1. If True, checks weights_location -- if non empty uses that;
             otherwise compute weights from scratch and store them at weights_location
         - weights_location: (path) Location to load from/save to the sequence weights
         - preprocess_MSA: (bool) performs pre-processing of MSA to remove short fragments and positions that are not well covered.
@@ -52,9 +54,9 @@ class MSA_processing:
         self.create_all_singles()
 
     def gen_alignment(self):
-        """ Read training alignment and store basics in class instance """
+        """Read training alignment and store basics in class instance"""
         self.aa_dict = {}
-        for i,aa in enumerate(self.alphabet):
+        for i, aa in enumerate(self.alphabet):
             self.aa_dict[aa] = i
 
         self.seq_name_to_sequence = defaultdict(str)
@@ -64,66 +66,73 @@ class MSA_processing:
                 line = line.rstrip()
                 if line.startswith(">"):
                     name = line
-                    if i==0:
+                    if i == 0:
                         self.focus_seq_name = name
                 else:
                     self.seq_name_to_sequence[name] += line
 
-        
         ## MSA pre-processing to remove inadequate columns and sequences
         if self.preprocess_MSA:
-            msa_df = pd.DataFrame.from_dict(self.seq_name_to_sequence, orient='index', columns=['sequence'])
+            msa_df = pd.DataFrame.from_dict(self.seq_name_to_sequence, orient="index", columns=["sequence"])
             # Data clean up
-            msa_df.sequence = msa_df.sequence.apply(lambda x: x.replace(".","-")).apply(lambda x: ''.join([aa.upper() for aa in x]))
+            msa_df.sequence = msa_df.sequence.apply(lambda x: x.replace(".", "-")).apply(lambda x: "".join([aa.upper() for aa in x]))
             # Remove columns that would be gaps in the wild type
-            non_gap_wt_cols = [aa!='-' for aa in msa_df.sequence[self.focus_seq_name]]
-            msa_df['sequence'] = msa_df['sequence'].apply(lambda x: ''.join([aa for aa,non_gap_ind in zip(x, non_gap_wt_cols) if non_gap_ind]))
-            assert 0.0 <= self.threshold_sequence_frac_gaps <= 1.0,"Invalid fragment filtering parameter"
-            assert 0.0 <= self.threshold_focus_cols_frac_gaps <= 1.0,"Invalid focus position filtering parameter"
+            non_gap_wt_cols = [aa != "-" for aa in msa_df.sequence[self.focus_seq_name]]
+            msa_df["sequence"] = msa_df["sequence"].apply(lambda x: "".join([aa for aa, non_gap_ind in zip(x, non_gap_wt_cols) if non_gap_ind]))
+            assert 0.0 <= self.threshold_sequence_frac_gaps <= 1.0, "Invalid fragment filtering parameter"
+            assert 0.0 <= self.threshold_focus_cols_frac_gaps <= 1.0, "Invalid focus position filtering parameter"
             msa_array = np.array([list(seq) for seq in msa_df.sequence])
-            gaps_array = np.array(list(map(lambda seq: [aa=='-' for aa in seq], msa_array)))
+            gaps_array = np.array(list(map(lambda seq: [aa == "-" for aa in seq], msa_array)))
             # Identify fragments with too many gaps
             seq_gaps_frac = gaps_array.mean(axis=1)
             seq_below_threshold = seq_gaps_frac <= self.threshold_sequence_frac_gaps
-            print("Proportion of sequences dropped due to fraction of gaps: "+str(round(float(1 - seq_below_threshold.sum()/seq_below_threshold.shape)*100,2))+"%")
+            print(
+                "Proportion of sequences dropped due to fraction of gaps: "
+                + str(round(float(1 - seq_below_threshold.sum() / seq_below_threshold.shape) * 100, 2))
+                + "%"
+            )
             # Identify focus columns
             columns_gaps_frac = gaps_array[seq_below_threshold].mean(axis=0)
             index_cols_below_threshold = columns_gaps_frac <= self.threshold_focus_cols_frac_gaps
-            print("Proportion of non-focus columns removed: "+str(round(float(1 - index_cols_below_threshold.sum()/index_cols_below_threshold.shape)*100,2))+"%")
+            print(
+                "Proportion of non-focus columns removed: "
+                + str(round(float(1 - index_cols_below_threshold.sum() / index_cols_below_threshold.shape) * 100, 2))
+                + "%"
+            )
             # Lower case non focus cols and filter fragment sequences
-            msa_df['sequence'] = msa_df['sequence'].apply(lambda x: ''.join([aa.upper() if upper_case_ind else aa.lower() for aa, upper_case_ind in zip(x, index_cols_below_threshold)]))
+            msa_df["sequence"] = msa_df["sequence"].apply(
+                lambda x: "".join([aa.upper() if upper_case_ind else aa.lower() for aa, upper_case_ind in zip(x, index_cols_below_threshold)])
+            )
             msa_df = msa_df[seq_below_threshold]
             # Overwrite seq_name_to_sequence with clean version
             self.seq_name_to_sequence = defaultdict(str)
-            for seq_idx in range(len(msa_df['sequence'])):
+            for seq_idx in range(len(msa_df["sequence"])):
                 self.seq_name_to_sequence[msa_df.index[seq_idx]] = msa_df.sequence[seq_idx]
 
         self.focus_seq = self.seq_name_to_sequence[self.focus_seq_name]
-        self.focus_cols = [ix for ix, s in enumerate(self.focus_seq) if s == s.upper() and s!='-'] 
+        self.focus_cols = [ix for ix, s in enumerate(self.focus_seq) if s == s.upper() and s != "-"]
         self.focus_seq_trimmed = [self.focus_seq[ix] for ix in self.focus_cols]
         self.seq_len = len(self.focus_cols)
         self.alphabet_size = len(self.alphabet)
 
         # Connect local sequence index with uniprot index (index shift inferred from 1st row of MSA)
         focus_loc = self.focus_seq_name.split("/")[-1]
-        start,stop = focus_loc.split("-")
+        start, stop = focus_loc.split("-")
         self.focus_start_loc = int(start)
         self.focus_stop_loc = int(stop)
-        self.uniprot_focus_col_to_wt_aa_dict \
-            = {idx_col+int(start):self.focus_seq[idx_col] for idx_col in self.focus_cols} 
-        self.uniprot_focus_col_to_focus_idx \
-            = {idx_col+int(start):idx_col for idx_col in self.focus_cols} 
+        self.uniprot_focus_col_to_wt_aa_dict = {idx_col + int(start): self.focus_seq[idx_col] for idx_col in self.focus_cols}
+        self.uniprot_focus_col_to_focus_idx = {idx_col + int(start): idx_col for idx_col in self.focus_cols}
 
         # Move all letters to CAPS; keeps focus columns only
-        for seq_name,sequence in self.seq_name_to_sequence.items():
-            sequence = sequence.replace(".","-")
+        for seq_name, sequence in self.seq_name_to_sequence.items():
+            sequence = sequence.replace(".", "-")
             self.seq_name_to_sequence[seq_name] = [sequence[ix].upper() for ix in self.focus_cols]
 
         # Remove sequences that have indeterminate AA (e.g., B, J, X, Z) in the focus columns
         if self.remove_sequences_with_indeterminate_AA_in_focus_cols:
             alphabet_set = set(list(self.alphabet))
             seq_names_to_remove = []
-            for seq_name,sequence in self.seq_name_to_sequence.items():
+            for seq_name, sequence in self.seq_name_to_sequence.items():
                 for letter in sequence:
                     if letter not in alphabet_set and letter != "-":
                         seq_names_to_remove.append(seq_name)
@@ -133,32 +142,34 @@ class MSA_processing:
                 del self.seq_name_to_sequence[seq_name]
 
         # Encode the sequences
-        print ("Encoding sequences")
-        self.one_hot_encoding = np.zeros((len(self.seq_name_to_sequence.keys()),len(self.focus_cols),len(self.alphabet)))
-        for i,seq_name in enumerate(self.seq_name_to_sequence.keys()):
+        print("Encoding sequences")
+        self.one_hot_encoding = np.zeros((len(self.seq_name_to_sequence.keys()), len(self.focus_cols), len(self.alphabet)))
+        for i, seq_name in enumerate(self.seq_name_to_sequence.keys()):
             sequence = self.seq_name_to_sequence[seq_name]
-            for j,letter in enumerate(sequence):
-                if letter in self.aa_dict: 
+            for j, letter in enumerate(sequence):
+                if letter in self.aa_dict:
                     k = self.aa_dict[letter]
-                    self.one_hot_encoding[i,j,k] = 1.0
+                    self.one_hot_encoding[i, j, k] = 1.0
 
         if self.use_weights:
             try:
                 self.weights = np.load(file=self.weights_location)
                 print("Loaded sequence weights from disk")
             except:
-                print ("Computing sequence weights")
+                print("Computing sequence weights")
                 list_seq = self.one_hot_encoding
                 list_seq = list_seq.reshape((list_seq.shape[0], list_seq.shape[1] * list_seq.shape[2]))
+
                 def compute_weight(seq):
-                    number_non_empty_positions = np.dot(seq,seq)
-                    if number_non_empty_positions>0:
-                        denom = np.dot(list_seq,seq) / np.dot(seq,seq) 
-                        denom = np.sum(denom > 1 - self.theta) 
-                        return 1/denom
+                    number_non_empty_positions = np.dot(seq, seq)
+                    if number_non_empty_positions > 0:
+                        denom = np.dot(list_seq, seq) / np.dot(seq, seq)
+                        denom = np.sum(denom > 1 - self.theta)
+                        return 1 / denom
                     else:
-                        return 0.0 #return 0 weight if sequence is fully empty
-                self.weights = np.array(list(map(compute_weight,list_seq)))
+                        return 0.0  # return 0 weight if sequence is fully empty
+
+                self.weights = np.array(list(map(compute_weight, list_seq)))
                 np.save(file=self.weights_location, arr=self.weights)
         else:
             # If not using weights, use an isotropic weight matrix
@@ -168,9 +179,9 @@ class MSA_processing:
         self.Neff = np.sum(self.weights)
         self.num_sequences = self.one_hot_encoding.shape[0]
 
-        print ("Neff =",str(self.Neff))
-        print ("Data Shape =",self.one_hot_encoding.shape)
-    
+        print("Neff =", str(self.Neff))
+        print("Data Shape =", self.one_hot_encoding.shape)
+
     def create_all_singles(self):
         start_idx = self.focus_start_loc
         focus_seq_index = 0
@@ -178,20 +189,20 @@ class MSA_processing:
         list_valid_mutations = []
         # find all possible valid mutations that can be run with this alignment
         alphabet_set = set(list(self.alphabet))
-        for i,letter in enumerate(self.focus_seq):
+        for i, letter in enumerate(self.focus_seq):
             if letter in alphabet_set and letter != "-":
                 for mut in self.alphabet:
-                    pos = start_idx+i
+                    pos = start_idx + i
                     if mut != letter:
-                        mutant = letter+str(pos)+mut
+                        mutant = letter + str(pos) + mut
                         self.mutant_to_letter_pos_idx_focus_list[mutant] = [letter, pos, focus_seq_index]
                         list_valid_mutations.append(mutant)
-                focus_seq_index += 1   
+                focus_seq_index += 1
         self.all_single_mutations = list_valid_mutations
 
     def save_all_singles(self, output_filename):
         with open(output_filename, "w") as output:
-            output.write('mutations')
+            output.write("mutations")
             for mutation in self.all_single_mutations:
-                output.write('\n')
+                output.write("\n")
                 output.write(mutation)
